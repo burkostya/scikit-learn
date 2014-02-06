df433f3f4f6a574bd36fbb8535b5818d088bde4a
Konstantin Burykin <burkostya@gmail.com>
2014-02-06 22:27:19 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# scikit-zero
+
+Scikit-learn wrapper for node.js over ZeroRPC
+
+## Installation
+
+`npm install scikit-zero`
+
+## Usage
+
+`var module = require('scikit-zero')`
+
+## API
+
+## Example
---
+++ test/scikit-zero.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+var modules = require('../lib/scikit-zero');
+
+describe('scikit-zero', function(){
+  it('should do something', function(){
+    expect(true).to.be.not.ok;
+  });
+});
