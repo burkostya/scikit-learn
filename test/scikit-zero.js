a49f32de3f892f8c219bd48d0dbe03b81cd00be5
Konstantin Burykin <burkostya@gmail.com>
2014-02-09 20:54:37 +0400

    add createClient

--- test/scikit-zero.js
+++ test/scikit-zero.js
@@ -7,7 +7,7 @@ describe('scikit-zero', function (){
   it('should create server and close it', function (done){
     var scikit = new Scikit();
     var server = scikit.createServer({
-      host: '0.0.0.0'
+      hostname: '0.0.0.0'
     });
     server.on('error', function (err) {
       server.close();
@@ -16,6 +16,30 @@ describe('scikit-zero', function (){
     server.listen(6001, function () {
       var client = new zerorpc.Client();
       client.connect('tcp://0.0.0.0:6001');
+      client.invoke('ping', function (err, res) {
+        client.close();
+        expect(err).to.not.exists;
+        expect(res).to.equal('pong');
+        server.close(function (err) {
+          expect(err).to.not.exists;
+          done();
+        });
+      });
+    });
+  });
+  it('should create client and close it', function (done) {
+    var scikit = new Scikit();
+    var server = scikit.createServer({
+      hostname: '0.0.0.0'
+    });
+    server.on('error', function (err) {
+      server.close();
+      done(err);
+    });
+    server.listen(6001, function () {
+      var client = scikit.createClient({
+        url: 'tcp://0.0.0.0:6001'
+      });
       client.invoke('ping', function (err, res) {
         client.close();
         expect(err).to.not.exists;
