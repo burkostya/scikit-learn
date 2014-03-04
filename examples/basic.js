14e8edbf302963f8f91ae5a8f284820610e71aa0
Konstantin Burykin <burkostya@gmail.com>
2014-03-05 02:29:06 +0400

    simplify example

--- examples/basic.js
+++ examples/basic.js
@@ -1,6 +1,3 @@
-var domain = require('domain');
-
-var trace = require('jstrace');
 var inspect = require('inspect-stream');
 
 var arrayify = require('arrayify-merge.s');
@@ -11,31 +8,21 @@ var scikit = new Scikit();
 
 var X = scikit.dataset('digits');
 var y = scikit.dataset('digits.target');
-var xyify = arrayify({ trace: trace });
+var xyify = arrayify();
 X.pipe(xyify);
 y.pipe(xyify);
+xyify
+  .pipe(inspect())
+  .on('error', function (err) {
+    console.log(err);
+  })
+  .on('end', function () {
+    console.log('end');
+  });
 //var k = kk
 /*var svc = scikit.svc('fit', {*/
   //gamma: 0.001,
   //C:     100
 /*});*/
 
-//var log = inspect();
-//xyify.pipe(log);
-//var target = scilit.digits('target');
-xyify.on('readable', function () {
-  //y.read();
-  console.log(xyify.read());
-})
-.on('end', function () {
-  console.log('end');
-  exit();
-})
-.on('error', function (err) {
-  exit(err);
-});
 
-function exit(err) {
-  if (err) { console.log(require('util').inspect(err.stack || err, true, 4, true)); }
-  console.log('exit');
-}
